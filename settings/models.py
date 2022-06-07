50784113e449b2c4587a31a342f207289e9960fc
Lukasz <lukgocek@gmail.com>
2022-06-07 22:52:16 +0100

    more model changes

--- settings/models.py
+++ settings/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 class ClassOfBusiness(models.Model):
     classOfBusiness_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.classOfBusiness_text
 
 class Department(models.Model):
     classOfBusiness = models.ForeignKey(ClassOfBusiness, on_delete=models.CASCADE)
     department_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.department_text
 
