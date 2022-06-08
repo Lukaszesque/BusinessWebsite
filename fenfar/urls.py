de8f9fe38fc422e903a88e0a6e11a2404bfc8a79
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-06-08 09:23:04 +0100

    more view changes

--- fenfar/urls.py
+++ fenfar/urls.py
@@ -16,6 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from settings import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # ex: /fenfar/
+    path('', views.index, name='index'),
+
+    #ex: /fenfar/5
+    path('<int:classOfBusiness_id>/', views.detail, name='detail'),
+
+    #ex /fenfar/5/results
+    path('<int:classOfBusiness_id>/results/', views.results, name='results'),
+
+    #ex /fenfar/5/vote/
+    path('<int:classOfBusiness_id>/vote/', views.vote, name='vote')
 ]
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,17 @@
+from urllib import response
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Welcome to the index page.")
+
+def detail(request, classOfBusiness_id):
+    return HttpResponse("You're looking at class of business %s." %classOfBusiness_id)
+
+def results(request, classOfBusiness_id):
+    response = "You're looking at the voting results of class of business %s."
+    return HttpResponse(response % classOfBusiness_id)
+
+def vote(request, classOfBusiness_id):
+    return HttpResponse("You're voting on class of business 1 %s." %classOfBusiness_id)
