d00e502c16b3c7f73a79f879c2483d7cfb08461e
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-06-16 17:49:20 +0100

    Changes to the class of business styling

--- settings/forms.py
+++ settings/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.forms import TextInput
 from .models import ClassOfBusiness
 
 class classOfBusinessForm(forms.ModelForm):
     class Meta:
         model = ClassOfBusiness
-        fields = "__all__"
+        fields = ["classOfBusiness_text"]
+        labels = {"classOfBusiness_text": "Class of Business name"}
+        widgets = {
+            'classOfBusiness_text': TextInput(attrs={
+                'class': "form-control",
+                'placeholder': "Type here..."
+            }) 
+        }
