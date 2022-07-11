e35dc1cfdbcf27ceaed7ce3f6306904bb16a4d53
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-07-11 10:07:42 +0100

    removes commented out code

--- settings/views/departments/departments.py
+++ settings/views/departments/departments.py
@@ -16,17 +16,3 @@ def addRecord(request):
     department.save()
     return HttpResponseRedirect(reverse('department_index'))
 
-
-
-
-    # def add(request):
-    # context={}
-    
-    # form = classOfBusinessForm(request.POST or None)
-    # if form.is_valid():
-    #     form.save()
-    #     messages.success(request, "Class of Business added successfully!")
-    #     return HttpResponseRedirect(reverse('classOfBusiness_index'))
-
-    # context['form'] = form
-    # return render(request, "settings/classOfBusiness/add.html", context)
